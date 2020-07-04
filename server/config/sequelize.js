ccec963e73a31ff7bc7ab0226f6a7160facef885
Daniel Ilich <daniel.i@itfox-web.com>
2020-07-04 16:52:29 +0300

    Изменил настройки дефолтного порта приложения

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -1,28 +1,34 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": 'toor',
-    "database": "sutr_dev",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  development: {
+    username: 'root',
+    password: 'toor',
+    database: 'sutr_dev',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
   },
-  "test": {
-    "username": "root",
-    "password": 'toor',
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: 'root',
+    password: 'toor',
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
   },
-  "production": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+  },
 }
